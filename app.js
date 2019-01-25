3a13a6ed26a2b59d72832ffb286b9c9e5a3d3995
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-01-25 22:03:48 +0300

    Adding methods to read the input data
    In UIController module I wrote a function getInput to get inputed values (I returned an object with function because it's in IIFE module so that i can read it inside the module and than again returned an object to have acces to it in Controller module.
    Then i created new local var called DOMstrings to put all our selectors here, so that if there will be some changes in names of classes in html, we can simply changed it here and not seaching it in js code.
    Then again returned an object of this to have acces to it in Controller module (because in the Controller I also selected button class).
    And finally used all of this in the Controller throug creating vars and calling this functions.
    
    
    // UI CONTROLLER
    var UIController = (function () {
    
        var DOMstrings = {
                inputType: '.add__type',
                inputDescription: '.add__description',
                inputValue: '.add__value',
                inputBtn: '.add__btn'
            };
        
        return {
            getInput: function() {
                return {
                    type: document.querySelector(DOMstrings.inputType).value,
                    description: document.querySelector(DOMstrings.inputDescription).value,
                    value: document.querySelector(DOMstrings.inputValue).value
                };
            },
    
            getDOMstrings: function() {
                return DOMstrings;
            }
        };
    })();
    
    // GLOBAL APP CONTROLLER
    var controller = (function (budgetCtrl, UICtrl) {
    
        var DOM = UICtrl.getDOMstrings();
    
        var ctrlAddItem = function () {
            // 1. Get the filed input data
            var input = UICtrl.getInput();
            console.log(input);
    
            // 2. Add the item to the budget controller
            // 3. Add the item to the UI
            // 4. Calculate and update Budget
        };
    
        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);

--- app.js
+++ app.js
@@ -10,7 +10,27 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
+
+    var DOMstrings = {
+            inputType: '.add__type',
+            inputDescription: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+        };
     
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -20,33 +40,35 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-        document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    var DOM = UICtrl.getDOMstrings();
 
-        document.addEventListener('keypress', function (event) {
-            if (event.keyCode === 13 || event.which === 13) {
-                ctrlAddItem();
-            }
-        }); 
-	
-
-	var ctrlAddItem = function () {
+    var ctrlAddItem = function () {
         
-        // 1. Get the filed input data 
-
+        // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
-
         // 3. Add the item to the UI
 
-
         // 4. Calculate and update Budget
-
-
     };
 
 
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });  
+
+
 })(budgetController, UIController);
+
+
+
 
 
 
