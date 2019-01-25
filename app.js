af24bd2c342afc71ed595f73035bfb7dc3362fa4
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-01-25 14:58:13 +0300

    Adding Event Listeners to the App
    Add Event Listeners to the Controller so that when we click the button or press ENTER, we call function ctrlAddItem, where later will be doing futher steps: get the filed input data, add the item to the budget controller, add the item to the UI, calculate and update Budget.
    
    
            document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
            document.addEventListener('keypress', function (event) {
                if (event.keyCode === 13 || event.which === 13) {
                    ctrlAddItem();
                }
            }); 
    	
    	var ctrlAddItem = function () {
            // 1. Get the filed input data 
            // 2. Add the item to the budget controller
            // 3. Add the item to the UI
            // 4. Calculate and update Budget
        };

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ var UIController = (function () {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+        document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        }); 
+	
+
+	var ctrlAddItem = function () {
+        
+        // 1. Get the filed input data 
+
+
+        // 2. Add the item to the budget controller
+
+
+        // 3. Add the item to the UI
+
+
+        // 4. Calculate and update Budget
+
+
+    };
+
 
 })(budgetController, UIController);
 
